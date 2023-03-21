081c7504c07264eb62ef4789376a00f5c0b4f80e
pmmanuel315 <pmmanuel315@gmail.com>
2023-03-21 11:08:08 -1000

    29:50

--- app/imports/api/stuff/Stuff.js
+++ app/imports/api/stuff/Stuff.js
@@ -15,6 +15,7 @@ class StuffsCollection {
       name: String,
       quantity: Number,
       owner: String,
+      value: Number,
       condition: {
         type: String,
         allowedValues: ['excellent', 'good', 'fair', 'poor'],
